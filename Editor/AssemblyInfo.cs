cd66c40ff97b88345cf9e9bdb91a4c2a3d8507f7
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2022-02-11 20:21:55 +0000

    Dev/package cleanup (#4)
    * Renamed assembly defs and updated package name
    
    * Renamed to match the rest of the project

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 // Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#define XRTK_OCULUS_EDITOR
+#define REALITYTOOLKIT_METAPLATFORM_EDITOR
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyTitle("com.xrtk.oculus.editor")]
-[assembly: AssemblyCompany("XRTK")]
-[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyTitle("com.realitytoolkit.metaplatform.editor")]
+[assembly: AssemblyCompany("Reality Collective")]
+[assembly: AssemblyCopyright("Copyright (c) Reality Collective. All rights reserved.")]
