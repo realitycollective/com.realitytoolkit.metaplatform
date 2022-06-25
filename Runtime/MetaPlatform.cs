d0a78f3bfedc1c5a87e574a1fe6bea678b31d0fb
Dino Fejzagić <dino.f@live.de>
2022-06-26 01:14:28 +0200

    Update MetaPlatform.IsAvailable implementation

--- Runtime/MetaPlatform.cs
+++ Runtime/MetaPlatform.cs
@@ -1,21 +1,19 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using RealityToolkit.Definitions.Platforms;
 using RealityToolkit.Interfaces;
-using RealityToolkit.Meta.Plugins;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace RealityToolkit.Meta
 {
     [System.Runtime.InteropServices.Guid("DB1ACC26-EC8D-4BC6-AFCA-C51351B2DA2E")]
     public class MetaPlatform : BasePlatform
     {
-        private static readonly System.Version NoVersion = new System.Version();
-
-        /// <inheritdoc />
-        public override bool IsAvailable =>
-            !Application.isEditor && OculusApi.Version > NoVersion && OculusApi.Initialized;
+        private const string xrDisplaySubsystemDescriptorId = "oculus display";
+        private const string xrInputSubsystemDescriptorId = "oculus input";
 
         /// <inheritdoc />
         public override IMixedRealityPlatform[] PlatformOverrides { get; } =
@@ -24,10 +22,60 @@ namespace RealityToolkit.Meta
             new WindowsStandalonePlatform()
         };
 
-#if UNITY_EDITOR
         /// <inheritdoc />
-        public override bool IsBuildTargetAvailable => base.IsBuildTargetAvailable && OculusApi.Version > NoVersion;
+        public override bool IsAvailable
+        {
+            get
+            {
+                var displaySubsystems = new List<XRDisplaySubsystem>();
+                SubsystemManager.GetSubsystems(displaySubsystems);
+                var xrDisplaySubsystemDescriptorFound = false;
 
+                for (var i = 0; i < displaySubsystems.Count; i++)
+                {
+                    var displaySubsystem = displaySubsystems[i];
+                    if (displaySubsystem.SubsystemDescriptor.id.Equals(xrDisplaySubsystemDescriptorId) &&
+                        displaySubsystem.running)
+                    {
+                        xrDisplaySubsystemDescriptorFound = true;
+                    }
+                }
+
+                // The XR Display Subsystem is not available / running,
+                // the platform doesn't seem to be available.
+                if (!xrDisplaySubsystemDescriptorFound)
+                {
+                    return false;
+                }
+
+                var inputSubsystems = new List<XRInputSubsystem>();
+                SubsystemManager.GetSubsystems(inputSubsystems);
+                var xrInputSubsystemDescriptorFound = false;
+
+                for (var i = 0; i < inputSubsystems.Count; i++)
+                {
+                    var inputSubsystem = inputSubsystems[i];
+                    if (inputSubsystem.SubsystemDescriptor.id.Equals(xrInputSubsystemDescriptorId) &&
+                        inputSubsystem.running)
+                    {
+                        xrInputSubsystemDescriptorFound = true;
+                    }
+                }
+
+                // The XR Input Subsystem is not available / running,
+                // the platform doesn't seem to be available.
+                if (!xrInputSubsystemDescriptorFound)
+                {
+                    return false;
+                }
+
+                // Only if both, Display and Input XR Subsystems are available
+                // and running, the platform is considered available.
+                return true;
+            }
+        }
+
+#if UNITY_EDITOR
         /// <inheritdoc />
         public override UnityEditor.BuildTarget[] ValidBuildTargets { get; } =
         {
