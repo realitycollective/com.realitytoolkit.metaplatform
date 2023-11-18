759af8d43b7193514320d626f05c597d46a230aa
Dino Fejzagić <dino.f@live.de>
2023-11-18 12:39:35 +0100

    Update to boundary package (#34)
    * Bump core package and add boundary package dependency
    
    * Bump core

--- Runtime/BoundaryService/IMetaBoundaryServiceModule.cs
+++ Runtime/BoundaryService/IMetaBoundaryServiceModule.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using RealityToolkit.Boundary.Interfaces;
+using RealityToolkit.Boundary;
 
 namespace RealityToolkit.MetaPlatform.BoundaryService
 {
